56df404dccbe2aa1880d1337e585e237e678f5fb
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-07-22 12:41:58 +0300

    commit tree 1e0e7d2bfb3ae9f79cbd768f9d5f9ba64d24a9d3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,13 +10,21 @@ const linkStyles = {
   color: "white",
 };
 
-function Navbar({ setIsLoggedIn }) {
+function Navbar({ onLogout }) {
   const history = useHistory();
 
-  function handleLogout() {
-    setIsLoggedIn(false);
-    history.push("/login");
+
+  function handleClick() {
+
+    onLogout();
+    history.push("/login")
+    
   }
+
+  // function handleLogout() {
+  //   setIsLoggedIn(false);
+  //   history.push("/login");
+  // }
 
   return (
     <div>
@@ -53,7 +61,7 @@ function Navbar({ setIsLoggedIn }) {
       >
         Login
       </NavLink>
-      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleClick}>Logout</button>
     </div>
   );
 }
